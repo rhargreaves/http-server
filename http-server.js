c3c06b7d72ea12d8c6a44d898b09a59925881afd
rhargreaves <robert.hargreaves@7digital.com>
2015-08-13 14:27:24 +0100

    Initial commit

---
+++ http-server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+
+http.createServer(function (req, res) {
+	console.log("--------------------------- %s ----------------------------", new Date());
+	console.log("%s %s", req.method, req.url);
+	
+	var urldata = url.parse(req.url, true);
+	var headers = {'Content-type': 'text/plain', 'Cache-Control': 'no-cache, no-store'};
+	printHeaders(req.headers);
+
+	if(urldata.query.key) {
+		headers["Surrogate-Key"] = urldata.query.key;
+	}	
+	res.writeHead(200, headers);
+	res.end("Original Request URL: "+req.url);
+	console.log("Response: %s", res.statusCode);
+}).listen(9615);
+
+function printHeaders(headers) {
+	for(key in headers) {
+		console.log("%s: %s", key, headers[key]);
+	}
+}
